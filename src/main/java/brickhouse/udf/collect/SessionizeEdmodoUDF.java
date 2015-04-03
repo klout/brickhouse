aaa034ecd1d7f9c131e35f74e676164d6ef5bc70
Lance Norskog <lance@edmodo.com>
2015-04-03 15:53:34 -0700

    Final Edmodo changes for sessionize. Takes a unique long as session id, doesn't create own.

--- src/main/java/brickhouse/udf/collect/SessionizeEdmodoUDF.java
+++ src/main/java/brickhouse/udf/collect/SessionizeEdmodoUDF.java
@@ -20,6 +20,8 @@ package brickhouse.udf.collect;
 /**
  * Creates a session id for an index and a time stamp. Default session length is 30 minute = 1800000 milliseconds
  */
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.apache.hadoop.hive.ql.exec.Description;
@@ -27,45 +29,45 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 
 @Description(
 		name="sessionize_edmodo", 
-		value="_FUNC_(string, timestamp) - Returns a json tuple session id for the given id and ts(long). Optional third parameter to specify interval tolerance in milliseconds",
-		extended="SELECT _FUNC_(uid, ts), uid, ts, event_type from foo; {session_id:,session_time:,session_order:")
+		value="_FUNC_(string, timestamp) - Returns a json tuple session id for the given session id and ts(long). Optional third parameter to specify interval tolerance in milliseconds",
+		extended="SELECT _FUNC_(uid, ts), uid, ts, event_type from foo; {session_id:,session_time:,session_order:}")
 
 public class SessionizeEdmodoUDF extends UDF {
-		private String lastUid = null;
+		private Long lastSessionId = null;
 		private long order = -1;
 		private long origTS = -1;
 		private long lastTS = -1;
-		private String lastUUID = null;
 
 	  
-	  public String evaluate(String uid, long ts, int tolerance) {
-		  if (uid.equals(lastUid) && timeStampCompare(lastTS, ts, tolerance)) { 
-			  lastTS = ts;		
+	  public String evaluate(Long uid, long ts, int tolerance) {
+		  if (uid.equals(lastSessionId) && timeStampCompare(lastTS, ts, tolerance)) { 
+			  lastTS = ts;	
 			  order++;
-		  } else if (uid.equals(lastUid)) {
+		  } else if (uid.equals(lastSessionId)) {
 			  lastTS = ts;
 			  origTS = ts;
-			  lastUUID=UUID.randomUUID().toString();
 			  order = 1;
 		  } else {
-			  lastUid = uid;
+			  lastSessionId = uid;
 			  lastTS = ts;
 			  origTS = ts;
-			  lastUUID=UUID.randomUUID().toString();
 			  order = 1;
 		  }
 		  StringBuilder sb = new StringBuilder();
-		  sb.append("{\"session_id\":\"");
-		  sb.append(lastUid);
-		  sb.append("\",\"session_time\":");
+		  sb.append("{\"session_id\":");
+		  sb.append(lastSessionId);
+		  sb.append(",\"session_start\":");
 		  sb.append(Long.toString(origTS));
+		  sb.append("\",\"session_start\":");
+		  sb.append(Long.toString(lastTS));
 		  sb.append(",\"session_order\":");
 		  sb.append(Long.toString(order));
 		  sb.append("}");
-		  return lastUUID;
+		  return sb.toString();
 	  }
-	  public String evaluate(String uid,  long ts) { 
-		 return evaluate(uid, ts, 1800000);
+	  
+	  public String evaluate(Long session_id,  long ts) { 
+		 return evaluate(session_id, ts, 1800000);
 	  }
 	  
 	  private Boolean timeStampCompare(long lastTS, long ts, int ms) { 
